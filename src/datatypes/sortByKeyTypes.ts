927ec6ed1bd652e1d297c59fc1cbd6c8cc234739
Spencer Cole <spencerlewiscole@gmail.com>
2023-06-27 10:35:04 -0400

    fix key type, put types in line

--- src/datatypes/sortByKeyTypes.ts
+++
@@ -1,8 +0,0 @@
-export enum Direction {
-  ASC = "asc",
-  DESC = "desc",
-}
-
-export type ObjectInputType = {
-  [key: string]: any;
-};
---
+++ src/sortByKey2.ts
@@ -0,0 +1,77 @@
+// TRIED TO MAKE FASTER
+/**
+ * @param {{ [key: string]: any }[]} input array of objects
+ * @param {string} propertyName key of object to sort
+ * @param {"ASC" | "DESC"} options direction of sorting
+ * Sort an array of objects by a specified property name (key) in specified direction.
+ * Default direction: ascending.
+ */
+export function sortByKey<InputObject, KeyType extends keyof InputObject>(
+    input: InputObject[],
+    propertyName: KeyType,
+    options: "ASC" | "DESC" = "ASC"
+  ) {
+    if (input.length <= 1) {
+      return input;
+    }
+    //
+    const partition = (
+      arr: InputObject[],
+      low: number,
+      high: number
+    ): number => {
+      const pivot = arr[Math.floor((low + high) / 2)][propertyName];
+      let i = low;
+      let j = high;
+  
+      while (i <= j) {
+        if (options === "ASC") {
+          while (arr[i][propertyName] < pivot) {
+            i++;
+          }
+          while (arr[j][propertyName] > pivot) {
+            j--;
+          }
+        }
+        if (options === "DESC") {
+          while (arr[i][propertyName] > pivot) {
+            i++;
+          }
+          while (arr[j][propertyName] < pivot) {
+            j--;
+          }
+        }
+        if (i <= j) {
+          [arr[i], arr[j]] = [arr[j], arr[i]];
+          i++;
+          j--;
+        }
+      }
+  
+      return i;
+    };
+    //
+    const quicksort = (
+      arr: InputObject[],
+      low: number,
+      high: number
+    ) => {
+      if (arr.length <= 1 || low >= high) {
+        return;
+      }
+      const index = partition(arr, low, high);
+  
+      if (low < index - 1) {
+        quicksort(arr, low, index - 1);
+      }
+      if (index < high) {
+        quicksort(arr, index, high);
+      }
+    };
+  
+    const sortedArray = [...input];
+    quicksort(sortedArray, 0, sortedArray.length - 1);
+  
+    return sortedArray;
+  }
+  
