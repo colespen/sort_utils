5f73a9ba03f2b2bc8d46335aca0d4dacdf901b48
Spencer Cole <spencerlewiscole@gmail.com>
2023-06-28 10:51:08 -0400

    fix lookup bug imporove space complexity in hash map

--- src/searchByField.ts
+++ src/searchByField.ts
@@ -2,7 +2,6 @@
  * @param {InputObject[]} input array of objects
  * @param {KeyType} propertyName key of object to search for
  * @param {KeyValue} propertyValue value of key to search for
- * @param {"ASC" | "DESC"} sortDirection direction of sorting. Default: Ascending
  * @returns {InputObject[] | []} object found or empty array
  * @description Search using hash map to locate object(s) in an array
  * by the specified property name (key) and value.
@@ -25,21 +24,21 @@ export function _searchByField<InputObject, KeyType extends keyof InputObject>(
   }
 
   const resultMap = new Map();
-
   // Build hash table by iterating over input
   for (const obj of input) {
     const fieldValue = obj[propertyName];
 
-    if (fieldValue) {
-      if (!resultMap.has(fieldValue)) {
-        resultMap.set(fieldValue, []);
-      }
-      resultMap.get(fieldValue).push(obj);
+    if (fieldValue && fieldValue === propertyValue) {
+      // push all matching objects to the same array in Map
+      const objectMatch = resultMap.get(fieldValue) || [];
+      objectMatch.push(obj);
+      resultMap.set(fieldValue, objectMatch);
     }
   }
 
   // Retrieve the matching objects from the hash table
   const resultArray = resultMap.get(propertyValue) || [];
 
+  // console.log(resultMap);
   return resultArray;
 }
